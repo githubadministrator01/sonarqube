d9f5b9acf8923c1daf6d6f98bc02efee8996f4c1
Petar Vasilev <petar.vasilev042@abv.bg>
2024-02-09 23:35:16 +0200

    Update README and values files

--- vagrant/set_dns.sh
+++ vagrant/set_dns.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Add local DNS resolution
 echo "192.168.99.100 ubuntu-server.local ubuntu-server" >> /etc/hosts
-echo "192.168.99.100 sonarqube-dce" >> /etc/hosts
+echo "192.168.99.100 sonarqube.test.local.com" >> /etc/hosts
